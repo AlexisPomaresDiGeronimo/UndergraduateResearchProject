5e10ba42734312282b0da5df78780d33d74a9e74
AlexisPomaresDiGeronimo <alexispomares24@hotmail.com>
2023-04-06 17:48:11 +0300

    adding content in home.html and correcting accommondatiob.html

--- Undergraduate_frontend/src/app/pages/accommondation/accommondation.component.ts
+++ Undergraduate_frontend/src/app/pages/accommondation/accommondation.component.ts
@@ -1,4 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ApiUrlService } from 'src/app/services/apiservice.service';
+
+interface Accommondation {
+  Id : number,
+  Title : string ,
+  Type  : string ,
+  Description : string ,
+  Location : string , 
+  Phone :  string ,
+  Opened : boolean 
+}
 
 @Component({
   selector: 'app-accommondation',
@@ -6,5 +18,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./accommondation.component.css']
 })
 export class AccommondationComponent {
-
+  accommondations : Accommondation[] | undefined;
+constructor(private http: HttpClient, private apiUrlService : ApiUrlService) {}
+ngOnInit() : void{
+  this.http.get(this.apiUrlService.getUrl() +'Accommondation').subscribe(
+    (data : any) => {
+      this.accommondations = data
+      console.log(this.accommondations)
+    }
+  )
+};
 }
--- Undergraduate_frontend/src/app/services/apiservice.service.spec.ts
+++ Undergraduate_frontend/src/app/services/apiservice.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiserviceService } from './apiservice.service';
+import { ApiUrlService } from './apiservice.service';
 
 describe('ApiserviceService', () => {
-  let service: ApiserviceService;
+  let service: ApiUrlService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiserviceService);
+    service = TestBed.inject(ApiUrlService);
   });
 
   it('should be created', () => {
